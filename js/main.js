148f93feccf69821eb5e8823a903f52827f52ec9
Peregrin <peregrinirus@mail.ru>
2022-09-10 20:52:38 +0300

    Функция, возвращающая случайное целое число из переданного диапазона включительно

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const getRandomInt = (startRange, endRange) => {
+  if (startRange<0 && endRange<0) {
+    return 'Неверные данные';
+  }
+  if(startRange>endRange){[startRange, endRange] = [endRange, startRange];}
+  if (startRange<0) {
+    startRange = 0;
+  }
+  if (endRange<0){
+    endRange = 0;
+  }
+  startRange = Math.ceil(startRange);
+  endRange = Math.floor(endRange);
+  if (startRange > endRange){
+    return 'Диапазон не содержит целочисленные значения';}
+  else {
+    return Math.floor(Math.random() * (endRange - startRange +1))+ startRange;
+  }
+};
+getRandomInt(2,8);
